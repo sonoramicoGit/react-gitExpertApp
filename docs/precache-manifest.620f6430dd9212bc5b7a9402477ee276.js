f7068bfce391ef5e43c8ed8885ed33227fd7626d
reysoac <rromeroprogramacion@gmail.com>
2022-02-24 10:29:59 -0600

    Subida de mi build como Docs

---
+++ docs/precache-manifest.620f6430dd9212bc5b7a9402477ee276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1436c8166843df6ee105c95e500bf84c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3130b6ef2e6365580789",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3130b6ef2e6365580789",
+    "url": "/static/js/main.e65ec879.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
